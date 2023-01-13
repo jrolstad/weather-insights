e040426fa1b5bc217b7bb808b5196a3559c0033a
Josh Rolstad <jrolstad@gmail.com>
2023-01-13 10:34:40 -0800

    Refactoring into data groups

--- internal/models/observation.go
+++ internal/models/observation.go
@@ -5,30 +5,49 @@ import (
 )
 
 type Observation struct {
-	At                            time.Time
-	Station                       *WeatherStation
-	RawData                       interface{}
-	Barometer                     float64
-	DailyRain                     float64
-	Dewpoint                      float64
-	DewpointIndoor                float64
-	EventRain                     float64
-	TemperatureFeelsLike          float64
-	TemperatureFeelsLikeIndoors   float64
-	HourlyRain                    float64
-	Humidity                      int
-	HumidityIndoors               int
-	LastRain                      time.Time
-	MaxDailyGust                  float64
-	Pm25                          float64
-	Pm25Daily                     float64
-	MonthlyRain                   float64
-	SolarRadiation                float64
-	TemperatureFahrenheit         float64
-	TemperatureFahrenheitInddors  float64
-	TotalRain                     float64
-	UvIndex                       float64
-	WeeklyRain                    float64
+	At          time.Time
+	Station     *WeatherStation
+	Rain        *RainObservation
+	Wind        *WindObservation
+	Solar       *SolarObservation
+	AirQuality  *AirQualityObservation
+	Temperature *TemperatureObservation
+	Humidity    *HumidityObservation
+	Pressure    *PressureObservation
+	RawData     interface{}
+}
+
+type PressureObservation struct {
+	Barometer float64
+}
+
+type HumidityObservation struct {
+	Dewpoint        float64
+	DewpointIndoor  float64
+	Humidity        int
+	HumidityIndoors int
+}
+
+type TemperatureObservation struct {
+	TemperatureFeelsLike         float64
+	TemperatureFeelsLikeIndoors  float64
+	TemperatureFahrenheit        float64
+	TemperatureFahrenheitInddors float64
+}
+
+type AirQualityObservation struct {
+	Pm25Inddor      int
+	Pm25DailyIndoor int
+	Pm25            float64
+	Pm25Daily       float64
+}
+
+type SolarObservation struct {
+	SolarRadiation float64
+	UvIndex        float64
+}
+
+type WindObservation struct {
 	WindDirection                 int
 	WindGust                      float64
 	WindGustDirection             int
@@ -37,7 +56,17 @@ type Observation struct {
 	WindSpeedAverage2Minutes      float64
 	WindDirectionAverage10Minutes int
 	WindSpeedAverage10Minutes     float64
-	YearlyRain                    float64
-	Pm25Inddor                    int
-	Pm25DailyIndoor               int
+	MaxDailyGust                  float64
+}
+
+type RainObservation struct {
+	DailyRain    float64
+	EventRain    float64
+	HourlyRain   float64
+	LastRain     time.Time
+	MonthlyRain  float64
+	TotalRain    float64
+	YearlyRain   float64
+	WeeklyRain   float64
+	MaxDailyGust float64
 }
