e9b028f2be006867747f44911516dc3e73d9d9c4
Josh Rolstad <jrolstad@gmail.com>
2023-01-13 12:57:08 -0800

    Adding observation table

---
+++ deployments/infra/glue_db_observation_table.tf
@@ -0,0 +1,36 @@
+esource "aws_glue_catalog_table" "observeration" {
+  name          = "observation"
+  database_name = aws_glue_catalog_database.object_db.name
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    EXTERNAL              = "TRUE"
+  }
+
+  storage_descriptor {
+    location      = "s3://${local.observation_bucket_name}"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name                  = "json"
+      serialization_library = "org.openx.data.jsonserde.JsonSerDe"
+
+      parameters = {
+        "serialization.format" = 1
+      }
+    }
+
+    columns {
+      name = "At"
+      type = "string"
+    }
+
+
+    columns {
+      name = "station"
+      type = "struct<macaddress:string,name:string>"
+    }
+  }
+}
